b8212cb78e9a0221cdc664c49a29d946c27f0af1
Eddie Zaneski <eddiezane@gmail.com>
2013-09-21 18:57:17 +0000

    Add rooms and shit @stewsmith is awesome

--- app.js
+++ app.js
@@ -4,12 +4,14 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var app = express();
 var http = require('http');
 var path = require('path');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var routes = require('./routes');
+var user = require('./routes/user');
 
-var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +32,17 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('test');
+  socket.on('join', function(sessionID) {
+    socket.set('sessionID', sessionID, function() {
+      if(socket.join(sessionID))
+        socket.broadcast.to(sessionID).emit('joined', io.sockets.clients(sessionID) );
+
+    });
+  });
 });
