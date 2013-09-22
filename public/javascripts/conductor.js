c3b8c0393d913cff1473562fabc2178166de1af6
Stewart Smith <stewartlsmith12@gmail.com>
2013-09-22 15:23:59 +0000

    emit active notes from conductor

--- public/javascripts/conductor.js
+++ public/javascripts/conductor.js
@@ -1,3 +1,11 @@
-function conductorClick(e) {
+var socket = io.connect('/');
 
+function conductorClick(e) {
+    var activeNotes = new Array();
+    var inputNotes = document.getElementsByClassName('active');
+
+    for(var i =0; i < inputNotes.length; i++) {
+        activeNotes.push(inputNotes[i].id);
+    }
+    socket.emit('active', activeNotes);
 }
