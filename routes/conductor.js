a6de1bd12470a2e224447ed669f3bd4843c7a8e0
Stewart Smith <stewartlsmith12@gmail.com>
2013-09-22 03:46:40 +0000

    make changes with conductor

--- routes/conductor.js
+++ routes/conductor.js
@@ -3,10 +3,6 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
-
 exports.conductor= function(req, res){
   res.render('conductor', { title: 'Express' });
 };
